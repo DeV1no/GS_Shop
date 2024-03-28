3fa297b2b5c6b16277567eb8b1c6bf32cf206dc9
Danial Shokouhmanesh <danialshokouhmanesh@outlook.com>
2024-03-28 13:53:52 +0330

    Add project files.

---
+++ GS_Shop_UserManagement/Core/GS_Shop_UserManagement.Domain/GS_Shop_UserManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace GS_Shop_UserManagement.Domain.Common;
+
+public class BaseEntity
+{
+    public BaseEntity()
+    {
+        DateCreated = DateTime.Now;
+    }
+
+    public int Id { get; set; }
+    public DateTime DateCreated { get; }
+    public string CreatedBy { get; } = string.Empty;
+    public DateTime LastModifiedDate { get; set; }
+    public string LastModifiedBy { get; } = string.Empty;
+}
---
+++ GS_Shop_UserManagement/Core/GS_Shop_UserManagement.Domain/GS_Shop_UserManagement.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using GS_Shop_UserManagement.Domain.Common;
+
+namespace GS_Shop_UserManagement.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
