90c0ac1940193437bb532b231a26da07785235da
Danial Shokouhmanesh <danialshokouhmanesh@outlook.com>
2024-04-01 14:31:55 +0330

    fix: fix some issue in smartLimitation

--- GS_Shop_UserManagement/Infrastructure/Persistence/GS_Shop_UserManagement.Persistence/SmartLimit/Service/SmartLimitationService.cs
+++ GS_Shop_UserManagement/Infrastructure/Persistence/GS_Shop_UserManagement.Persistence/SmartLimit/Service/SmartLimitationService.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq.Expressions;
 using Microsoft.AspNetCore.Http;
 using System.Security.Claims;
-using Microsoft.EntityFrameworkCore;
 using GS_Shop_UserManagement.Domain.Enums;
 
 namespace GS_Shop_UserManagement.Persistence.SmartLimit.Service;
@@ -13,51 +12,43 @@ public class SmartLimitationService<TEntity>(GSShopUserManagementDbContext dbCon
 {
     public IQueryable<TEntity> GetLimitedEntitiesQueryAsync()
     {
-        var userClaims = (httpContextAccessor.HttpContext?.User)
-                         ?? throw new InvalidOperationException("No user claims found.");
+        var entities = dbContext.Set<TEntity>();
+        var userClaims = httpContextAccessor.HttpContext?.User;
+        if (userClaims is null)
+            return entities;
 
         var entityType = typeof(TEntity);
         var limitationTag = GetLimitationTag(entityType);
         var limitationClaims = GetLimitationClaims(userClaims, limitationTag);
-
+        if (limitationClaims is null)
+            return entities;
         if (string.IsNullOrEmpty(limitationTag))
-        {
             throw new InvalidOperationException($"No limitation tag found for entity type {entityType.Name}.");
-        }
-
         var limitationClaimToIntList = limitationClaims.Value.Split(',')
             .Select(int.Parse)
             .ToList();
-
-        var entities = dbContext.Set<TEntity>();
-
         // Build expression for Id property
         var parameter = Expression.Parameter(typeof(TEntity), "entity");
         var property = Expression.Property(parameter, "Id");
-        var lambda = Expression.Lambda<Func<TEntity, int>>(property, parameter);
-
         // Compose the where clause dynamically
         var containsMethod = typeof(Enumerable).GetMethods()
             .Single(m => m.Name == "Contains" && m.GetParameters().Length == 2)
             .MakeGenericMethod(typeof(int));
-
         var constant = Expression.Constant(limitationClaimToIntList);
         var containsExpression = Expression.Call(containsMethod, constant, property);
         var whereExpression = Expression.Lambda<Func<TEntity, bool>>(containsExpression, parameter);
-
         // Apply the where clause
         var result = entities.Where(whereExpression);
-
         return result;
     }
 
 
 
 
-    private Claim GetLimitationClaims(ClaimsPrincipal userClaims, string limitationTag)
+    private Claim? GetLimitationClaims(ClaimsPrincipal userClaims, string limitationTag)
     {
         // Find all claims with a name ending with "Limitation" in the user's claims
-        return userClaims.Claims.First(c => c.Type.EndsWith("Limitation") && c.Type == limitationTag);
+        return userClaims.Claims.FirstOrDefault(c => c.Type.EndsWith("Limitation") && c.Type == limitationTag);
 
     }
 
