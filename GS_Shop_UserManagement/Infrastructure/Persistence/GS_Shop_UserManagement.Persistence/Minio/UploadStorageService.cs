156b1ef0b50a5296aab993662e4a78feb972053c
Danial <danialshokouhmanesh@gmail.com>
2024-05-02 20:50:08 +0330

    ref: minio upload service refactored

--- GS_Shop_UserManagement/Infrastructure/Persistence/GS_Shop_UserManagement.Persistence/Minio/UploadStorageService.cs
+++ GS_Shop_UserManagement/Infrastructure/Persistence/GS_Shop_UserManagement.Persistence/Minio/UploadStorageService.cs
@@ -4,96 +4,110 @@ using GS_Shop_UserManagement.Infrastructure.Helpers;
 using Microsoft.AspNetCore.Http;
 using Minio;
 using Minio.DataModel.Args;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace GS_Shop_UserManagement.Persistence.Minio;
-
-public class UploadStorageService<TEntity>(IMinioClient minioClient)
-    : IUploadStorageService<TEntity>
-    where TEntity : class
+namespace GS_Shop_UserManagement.Persistence.Minio
 {
-    public async Task<string> UploadFileAsync(IFormFile formFile,string? previousFilePath, 
-        CancellationToken cancellationToken = default)
+    public class UploadStorageService<TEntity> : IUploadStorageService<TEntity>
+        where TEntity : class
     {
-        if (!string.IsNullOrEmpty(previousFilePath))
-            await DeleteFileAsync(previousFilePath);
-            
-        var entityName = GetEntityAsStringHelper.GetEntityAsStrings<TEntity>().ToLower();
-        var isBucketExisted = await DoesBucketExistAsync(entityName);
-        if (!isBucketExisted)
-            await CreateBucketAsync(entityName, cancellationToken);
-        var bas64ConvertedFile = ConvertIFormFileToBase64(formFile);
-        await using var ms = new MemoryStream(Convert.FromBase64String(bas64ConvertedFile));
-        var fileName = $"{Guid.NewGuid():N}.{FileTypeValidator.GetFileType(ms).Extension}";
-        ms.Position = 0;
-        var fileExtension = GetFileExtension(fileName);
-        var createFileArgs = new PutObjectArgs()
-            .WithBucket(entityName)
-            .WithStreamData(ms)
-            .WithObjectSize(ms.Length)
-            .WithObject(fileName)
-            .WithContentType(!string.IsNullOrEmpty(fileExtension)
-                ? fileExtension
-                : "application/octet-stream");
+        private readonly IMinioClient _minioClient;
 
-        var response = await minioClient.PutObjectAsync(createFileArgs, cancellationToken: cancellationToken);
-        return entityName + "/" + response.ObjectName;
-    }
-
-   
-    
-    
-    private static string GetFileExtension(string fileName)
-    {
-        var lastDotIndex = fileName.LastIndexOf('.');
-        return fileName[(lastDotIndex + 1)..];
-    }
-   
-
-
-    private static string ConvertIFormFileToBase64(IFormFile file)
-    {
-        using var memoryStream = new MemoryStream();
-        file.CopyTo(memoryStream);
-        memoryStream.Position = 0; // Reset position to beginning
-        var bytes = memoryStream.ToArray();
-        return Convert.ToBase64String(bytes);
-    }
-
-    private async Task<bool> DoesBucketExistAsync(string bucketName)
-    {
-        try
+        public UploadStorageService(IMinioClient minioClient)
         {
-            return await minioClient.BucketExistsAsync(new BucketExistsArgs().WithBucket(bucketName));
+            _minioClient = minioClient;
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error checking bucket existence: {ex.Message}");
-            return false;
-        }
-    }
 
-    private async Task CreateBucketAsync(string name,
-        CancellationToken cancellationToken = default)
-    {
-        var createBucketArgs = new MakeBucketArgs().WithBucket(name);
-        await minioClient.MakeBucketAsync(createBucketArgs, cancellationToken);
-    }
-    private async Task<bool> DeleteFileAsync(string filePath)
-    {
-        var seperatedPath = BucketPathSeparator.Separat(filePath);
-        try
+        public async Task<string> UploadFileAsync(IFormFile formFile, string? previousFilePath,
+            CancellationToken cancellationToken = default)
         {
-           
-            var removeObjectArgs = new RemoveObjectArgs()
-                .WithBucket(seperatedPath.Item1)
-                .WithObject(seperatedPath.Item2);
-            await minioClient.RemoveObjectAsync(removeObjectArgs);
-            return true; 
+            if (!string.IsNullOrEmpty(previousFilePath))
+                await FileOperations.DeleteFileAsync(_minioClient, previousFilePath);
+
+            var entityName = GetEntityAsStringHelper.GetEntityAsStrings<TEntity>().ToLower();
+            var isBucketExisted = await MinioClientOperations.DoesBucketExistAsync(_minioClient, entityName);
+            if (!isBucketExisted)
+                await MinioClientOperations.CreateBucketAsync(_minioClient, entityName, cancellationToken);
+
+            var base64ConvertedFile = FileOperations.ConvertIFormFileToBase64(formFile);
+            await using var ms = new MemoryStream(Convert.FromBase64String(base64ConvertedFile));
+            var fileName = $"{Guid.NewGuid():N}.{FileTypeValidator.GetFileType(ms).Extension}";
+            ms.Position = 0;
+            var fileExtension = FileOperations.GetFileExtension(fileName);
+            var createFileArgs = new PutObjectArgs()
+                .WithBucket(entityName)
+                .WithStreamData(ms)
+                .WithObjectSize(ms.Length)
+                .WithObject(fileName)
+                .WithContentType(!string.IsNullOrEmpty(fileExtension)
+                    ? fileExtension
+                    : "application/octet-stream");
+
+            var response = await _minioClient.PutObjectAsync(createFileArgs, cancellationToken: cancellationToken);
+            return entityName + "/" + response.ObjectName;
         }
-        catch (Exception ex)
+
+        
+
+        
+    } 
+     static class FileOperations
+    {
+        public static string GetFileExtension(string fileName)
         {
-            Console.WriteLine($"Error deleting file: {ex.Message}");
-            return false; 
+            var lastDotIndex = fileName.LastIndexOf('.');
+            return fileName[(lastDotIndex + 1)..];
+        }
+
+        public static string ConvertIFormFileToBase64(IFormFile file)
+        {
+            using var memoryStream = new MemoryStream();
+            file.CopyTo(memoryStream);
+            memoryStream.Position = 0; // Reset position to beginning
+            var bytes = memoryStream.ToArray();
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static async Task<bool> DeleteFileAsync(IMinioClient minioClient, string filePath)
+        {
+            var separatedPath = BucketPathSeparator.Separat(filePath);
+            try
+            {
+                var removeObjectArgs = new RemoveObjectArgs()
+                    .WithBucket(separatedPath.Item1)
+                    .WithObject(separatedPath.Item2);
+                await minioClient.RemoveObjectAsync(removeObjectArgs);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deleting file: {ex.Message}");
+                return false;
+            }
+        }
+    }
+    static class MinioClientOperations
+    {
+        public static async Task<bool> DoesBucketExistAsync(IMinioClient minioClient, string bucketName)
+        {
+            try
+            {
+                return await minioClient.BucketExistsAsync(new BucketExistsArgs().WithBucket(bucketName));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error checking bucket existence: {ex.Message}");
+                return false;
+            }
+        }
+
+        public static async Task CreateBucketAsync(IMinioClient minioClient, string name, CancellationToken cancellationToken = default)
+        {
+            var createBucketArgs = new MakeBucketArgs().WithBucket(name);
+            await minioClient.MakeBucketAsync(createBucketArgs, cancellationToken);
         }
     }
 }
