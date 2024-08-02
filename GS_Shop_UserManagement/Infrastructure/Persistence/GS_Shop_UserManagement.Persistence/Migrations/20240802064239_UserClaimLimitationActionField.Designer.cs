b0a6a802c74760dea97eb1a7c4acee9b5c1587c8
Danial Shokouhmanesh <danialshokouhmanesh@outlook.com>
2024-08-02 10:13:25 +0330

    feat: user limitation claim action migrations added

--- GS_Shop_UserManagement/Infrastructure/Persistence/GS_Shop_UserManagement.Persistence/Migrations/20240802064239_UserClaimLimitationActionField.Designer.cs
+++ GS_Shop_UserManagement/Infrastructure/Persistence/GS_Shop_UserManagement.Persistence/Migrations/20240802064239_UserClaimLimitationActionField.Designer.cs
@@ -4,6 +4,7 @@ using GS_Shop_UserManagement.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GS_Shop_UserManagement.Persistence.Migrations
 {
     [DbContext(typeof(GSShopUserManagementDbContext))]
-    partial class GSShopUserManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240802064239_UserClaimLimitationActionField")]
+    partial class UserClaimLimitationActionField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GS_Shop_UserManagement/Infrastructure/Persistence/GS_Shop_UserManagement.Persistence/Migrations/20240802064239_UserClaimLimitationActionField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GS_Shop_UserManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserClaimLimitationActionField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Action",
+                table: "UserLimitationClaims",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Action",
+                table: "UserLimitationClaims");
+        }
+    }
+}
